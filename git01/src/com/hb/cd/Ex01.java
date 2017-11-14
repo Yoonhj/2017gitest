c66148cbdfb466bdd9de388913d944ff781471ac
Yoonhj <33647921+Yoonhj@users.noreply.github.com>
2017-11-14 17:52:41 +0900

    test

---
+++ git01/src/com/hb/cd/Ex01.java
@@ -0,0 +1,23 @@
+package com.hb.cd;
+
+public class Ex01 {
+	
+	public int sabun=0;
+	protected String name;
+	java.util.Date nalzza;
+	private int pay;
+	
+	
+	public void func01(){}
+	
+	private int func02(){
+		
+		return 0;
+	}
+	
+	protected void func03(int a){
+		
+	}
+	
+	
+}
